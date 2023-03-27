1df5ddc9a973308b1afb38264d05efce81496397
Onyango-Sangoro <129027111+Onyango-Sangoro@users.noreply.github.com>
2023-03-27 13:01:13 +0300

    Made init_EIR = 100

--- scripts/Sandal_test.R
+++ scripts/Sandal_test.R
@@ -3,7 +3,7 @@ model_0 <- run_model(model = "odin_model",
                      time = 730)
 plot(model_0$t,model_0$prev, main= "Prevalance", ylim = c(0, 1), type='l')
 model_1 <- run_model(model = "odin_model_Sandal",
-                      init_EIR = 10,
+                      init_EIR = 100,
                       time = 730)
 plot(model_1$t,model_1$prev, main= "Sandal_model_Prevalance", ylim = c(0, 1), type='l')
 ##each time you make a change to odin_model_Sandals, need to run: 
